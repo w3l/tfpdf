d37ad94a15808323c696aa6c983a31f1461b8940
w3l <w3l@users.noreply.github.com>
2020-11-02 13:28:29 +0100

    Update tFPDF.php
    get_magic_quotes_runtime() is deprecated as of PHP 7.4.0

--- src/tFPDF.php
+++ src/tFPDF.php
@@ -1257,8 +1257,10 @@ function _dochecks()
 	if(ini_get('mbstring.func_overload') & 2)
 		$this->Error('mbstring overloading must be disabled');
 	// Ensure runtime magic quotes are disabled
-	if(get_magic_quotes_runtime())
+	/*
+    if(get_magic_quotes_runtime())
 		@set_magic_quotes_runtime(0);
+    */
 }
 
 function _getfontpath()
